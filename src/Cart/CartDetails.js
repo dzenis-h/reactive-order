18128088a43d4eeea0890d245efe30709078e38e
BiggaHD <dzenis.hankusic@gmail.com>
2021-05-18 15:22:53 +0200

    Add Firebase as a dummy BE

--- src/Cart/CartDetails.js
+++ src/Cart/CartDetails.js
@@ -1,10 +1,16 @@
-import { useContext } from 'react';
+import { Fragment, useContext, useState } from 'react';
 import Modal from '../UI/Modal';
 import classes from './CartDetails.module.css';
 import CartContext from '../store/cart-context';
 import CartItem from './Cart';
+import Checkout from './Checkout';
+import axios from 'axios';
 
 const CartDetails = (props) => {
+  const [show, setShow] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+
   const ctx = useContext(CartContext);
 
   const hasItems = ctx.items.length > 0;
@@ -15,6 +21,20 @@ const CartDetails = (props) => {
 
   const cartItemAddHandler = (item) => {
     ctx.addItem({ ...item, amount: 1 });
+  };
+
+  const onConfirm = async (userData) => {
+    setIsSubmitting(true);
+    await axios.post(
+      'https://meals-data-default-rtdb.europe-west1.firebasedatabase.app/orders.json',
+      {
+        userData,
+        order: ctx.items,
+      }
+    );
+    // props.onClose();
+    setIsSubmitting(false);
+    setDidSubmit(true);
   };
 
   const cartItems = (
@@ -33,23 +53,55 @@ const CartDetails = (props) => {
     </ul>
   );
 
-  return (
-    <div>
-      <Modal>
-        {cartItems}
-        <div className={classes.total}>
-          <span>Total Amount</span>
-          <span className={classes.total}>{ctx.totalAmount.toFixed(2)}</span>
-        </div>
-        <div className={classes.actions}>
-          <button className={classes['button--alt']} onClick={props.onClose}>
-            Close
-          </button>
-          {hasItems && <button className={classes.button}>Order</button>}
-        </div>
-      </Modal>
-      )
+  const actions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button
+          className={classes.button}
+          onClick={() => setShow((prev) => !prev)}
+        >
+          Order
+        </button>
+      )}
     </div>
+  );
+
+  const modalContent = (
+    <Fragment>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span className={classes.total}>{ctx.totalAmount.toFixed(2)}</span>
+      </div>
+      {show && <Checkout onCancel={props.onClose} onConfirm={onConfirm} />}
+      {!show && actions}
+    </Fragment>
+  );
+
+  const submitting = (
+    <Fragment>
+      <p>Submitting Your Order.</p>
+    </Fragment>
+  );
+
+  const submitted = (
+    <div className={classes.actions}>
+      <p style={{ textAlign: 'center' }}>Your order has been submitted.</p>
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+    </div>
+  );
+
+  return (
+    <Modal>
+      {!isSubmitting && !didSubmit && modalContent}
+      {isSubmitting && submitting}
+      {didSubmit && submitted}
+    </Modal>
   );
 };
 
--- src/meals/MealsList.js
+++ src/meals/MealsList.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import { DUMMY_MEALS } from '../assets/dummy-data';
+import React, { useEffect, useState } from 'react';
 import classes from './MealsList.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
+import axios from 'axios';
 
 function MealsList() {
+  const [meals, setMeals] = useState([]);
+  const [error, setError] = useState('');
+
+  const getMeals = async () => {
+    try {
+      const { data } = await axios.get(
+        'https://meals-data-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+      );
+
+      const array = [];
+      for (const key in data) {
+        array.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(array);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getMeals();
+  }, []);
+
+  if (error) {
+    return <h4 className={classes.error}>{error}</h4>;
+  }
+
   return (
     <div className={classes.meals}>
       <Card>
         <ul>
-          {DUMMY_MEALS.map((m) => {
-            return (
-              <MealItem
-                key={m.id}
-                id={m.id}
-                name={m.name}
-                description={m.description}
-                price={m.price}
-              />
-            );
-          })}
+          {meals.length > 0 ? (
+            meals.map((m) => {
+              return (
+                <MealItem
+                  key={m.id}
+                  id={m.id}
+                  name={m.name}
+                  description={m.description}
+                  price={m.price}
+                />
+              );
+            })
+          ) : (
+            <p>Loading ...</p>
+          )}
         </ul>
       </Card>
     </div>
